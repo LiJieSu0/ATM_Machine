c6d90f448d642b77827fa85bac88cd410d8cbfd1
LiJie <opwinopwin@gmail.com>
2023-07-20 21:07:42 -0400

    Add project files.

---
+++ ATM_Machine/CardHolder.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_Machine
+{
+    internal class CardHolder
+    {
+        private string firstName;
+        private string lastName;
+        private string cardNumber;
+        private string pin;
+        private double balance;
+        public CardHolder(string firstName, string lastName, string cardNumber, string pin,double balance=0) { 
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.cardNumber = cardNumber;
+            this.pin = pin;
+            this.balance = balance;
+        }
+        public double getBalance()
+        {
+            return balance; 
+        }
+        public string getCardHolderName() { 
+            return firstName + " " + lastName;
+        }
+        public string getCardNumber()
+        {
+            return cardNumber;
+        }
+        public string getPin()
+        {
+            return pin;
+        }
+        public void withdraw(double amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Please enter a valid number");
+                return;
+            }
+            if (amount > balance)
+            {
+                Console.WriteLine("Balance is not enough");
+            }
+            else
+            {
+                balance -= amount;
+                Console.WriteLine("Withdraw success!");
+            }
+        }
+
+        public void deposit(double amount) {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Please enter a valid number");
+                return;
+            }
+            balance += amount;
+        }
+
+        public void checkBalance()
+        {
+
+            Console.WriteLine($"Current balance is ${formattedNumber(balance)}");
+        }
+        string formattedNumber(double number)
+        {
+            string formatted=number.ToString("F2");
+            return formatted;
+        }
+    }
+}
---
+++ ATM_Machine/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace ATM_Machine
+{
+    class ATM
+    {
+        public static void Main(string[] args)
+        {
+            CardHolder obj1 = new CardHolder("John", "Cena", "123456789", "000000");
+            int numberOfErrors=0;
+            while (true)
+            {
+                Console.WriteLine("Enter your card number");
+                string enteredCard=Console.ReadLine();
+                if (numberOfErrors >= 3)
+                {
+                    Console.WriteLine("Too many times error. Enter any key to leave");
+                    Environment.Exit(0);
+                }
+                if (enteredCard !=obj1.getCardNumber())
+                {
+                    Console.WriteLine("Wrong card number, please try again");
+                    numberOfErrors++;
+                    continue;
+                }
+
+                break;
+            }
+            numberOfErrors = 0;
+            while (true)
+            {
+                Console.WriteLine("Enter your pin");
+                string enteredPin = Console.ReadLine();
+                if (numberOfErrors >= 3)
+                {
+                    Console.WriteLine("Too many times error. Enter any key to leave");
+                    Environment.Exit(0);
+                }
+                if (enteredPin != obj1.getPin())
+                {
+                    Console.WriteLine("Wrong pin number, please try again");
+                    numberOfErrors++;
+                    continue;
+                }
+                break;
+            }
+            while (true)
+            {
+                Console.WriteLine($"Welcome {obj1.getCardHolderName()} ! What can I help you today?");
+                Console.WriteLine("1. Check Balance");
+                Console.WriteLine("2. Withdraw");
+                Console.WriteLine("3. Deposit");
+                Console.WriteLine("4. Leave");
+                int option=Int32.Parse(Console.ReadLine());
+                double amount = 0;
+                string input;
+                switch (option)
+                {
+                    case 1:
+                        obj1.checkBalance();
+                        break;
+                    case 2:
+                        Console.WriteLine("Please Enter amount you want to withdraw");
+                        input= Console.ReadLine();
+                        if (input!=null)
+                        {
+                            amount= Convert.ToDouble(input);
+                        }
+                        obj1.withdraw(amount);
+                        break;
+                    case 3:
+                        Console.WriteLine("Please Enter amount you want to withdraw");
+                        input = Console.ReadLine();
+                        if (input != null)
+                        {
+                            amount = Convert.ToDouble(input);
+                        }
+                        obj1.deposit(amount);
+                        break;
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                
+                }
+            }
+        }
+    }
+}
